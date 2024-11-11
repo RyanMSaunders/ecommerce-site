2ddfcf6a42faa4cb22acf168a34f3957ffbb37b4
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-11-11 11:31:47 -0500

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,18 @@ module.exports = {
     config.resolve.alias['react'] = require.resolve('react');
     return config;
   },
+  rules: [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    },
+  ],
 }
 
 export default nextConfig;
