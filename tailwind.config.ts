778c83daa33f68d5234ff43ee65c8f66a1fa5bbd
RyanMSaunders <ryan.s.maguire@gmail.com>
2024-11-11 20:30:35 -0500

    Add to tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,8 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/Components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+		'./pages/**/*.{js,ts,jsx,tsx}',
+  	'./components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
 		container: {
